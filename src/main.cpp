4085cb62b5f84f896254a231f26c81cf9e1d6c30
Yoonjoohwan <Yoonjoohwan121@gmail.com>
2024-11-28 12:09:28 +0900

    glfw 추가

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
 #include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv)
 {
-    SPDLOG_INFO("hello, opengl");
-    return 0;
+    SPDLOG_INFO("Start Program");
 
+    SPDLOG_INFO("");
+    if (!glfwInit())
+    {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
+        nullptr, nullptr);
+    
+    if (!window)
+    {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
