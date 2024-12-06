324d5af3081606509afadd686ec1664c1b74af2c
Yoonjoohwan <Yoonjoohwan121@gmail.com>
2024-12-06 21:03:38 +0900

    texture h cpp 추가 / 텍스쳐 2개 같이 적용해보기

--- src/context.cpp
+++ src/context.cpp
@@ -62,18 +62,20 @@ bool Context::Init()
     SPDLOG_INFO("image: {}x{}, {} channels",
                 image->GetWidth(), image->GetHeight(), image->GetChannelCount());
 
-    glGenTextures(1, &m_texture);
-    glBindTexture(GL_TEXTURE_2D, m_texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    m_texture = Texture::CreateFromImage(image.get());
+    
+    auto image2 = Image::Load("./images/awesomeface.png");
 
-    //텍스처 좌표 계의 x, y -> S, T
+    m_texture2 = Texture::CreateFromImage(image2.get());
 
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
-                 image->GetWidth(), image->GetHeight(), 0,
-                 GL_RGB, GL_UNSIGNED_BYTE, image->GetData());
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, m_texture->Get());
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, m_texture2->Get());
+
+    m_program->Use();
+    glUniform1i(glGetUniformLocation(m_program->Get(), "tex"), 0);
+    glUniform1i(glGetUniformLocation(m_program->Get(), "tex2"), 1);
 
     return true;
 }
--- src/context.h
+++ src/context.h
@@ -6,6 +6,7 @@
 #include "program.h"
 #include "buffer.h"
 #include "vertex_layout.h"
+#include "texture.h"
 
 CLASS_PTR(Context)
 class Context {
@@ -21,7 +22,8 @@ private:
     VertexLayoutUPtr m_vertexLayout;
     BufferUPtr m_vertexBuffer;
     BufferUPtr m_indexBuffer;
-    uint32_t m_texture; //texture id 저장을 위한 변수
+    TextureUPtr m_texture; //texture id 저장을 위한 변수
+    TextureUPtr m_texture2;
 };
 
 
